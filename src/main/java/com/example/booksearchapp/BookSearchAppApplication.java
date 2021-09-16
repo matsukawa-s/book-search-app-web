c544b9fc901693e5feee473737a138aa0061f7aa
matsukawa-s <syoudai.hand@gmail.com>
2021-09-16 21:33:41 +0900

    :tada: Initial commit

---
+++ src/main/java/com/example/booksearchapp/BookSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksearchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booksearchapp/BookSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksearchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
