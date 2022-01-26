801cfb3a47b1a8e0b4f10e83768c04490cf2d7dc
Shimoji Ryouma <tuuykgc6aap1hbdmrwnc@docomo.ne.jp>
2022-01-26 23:45:45 +0900

    :bug: フォルダー階層の見直し

--- api-server/docker/db/sql/02-categories.sql
+++
@@ -1,9 +0,0 @@
-create table categories
-(
-    id int unsigned auto_increment,
-    name varchar(50) not null,
-    id_deleted boolean default false not null,
-    sort_number int not null,
-    constraint categories_pk
-        primary key (id)
-);
--- api-server/docker/db/sql/03-label_groups.sql
+++
@@ -1,7 +0,0 @@
-create table label_groups
-(
-    id int unsigned auto_increment,
-    name varchar(200) not null,
-    constraint label_groups_pk
-        primary key (id)
-);
